56d39ff6336187f6b3a8e769dc76c32efd4d2442
PkQDark <blackbirdvlad@gmail.com>
2017-07-18 08:45:37 +0300

    ok

--- serv/tasks.py
+++ serv/tasks.py
@@ -12,7 +12,7 @@ from django.db.models import Sum
 from pytz import timezone
 import string
 from .celery import app
-#good news
+#new good news
 
 lock = threading.Lock()
 kiev = timezone('Europe/Kiev')
