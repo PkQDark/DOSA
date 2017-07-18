2ad38478a740c60a2a8823a7b43c3e5aaed76ffa
Anna <miaskad@gmail.com>
2017-07-18 09:27:08 +0300

    Ydsgsd

--- serv/tasks.py
+++ serv/tasks.py
@@ -12,7 +12,7 @@ from django.db.models import Sum
 from pytz import timezone
 import string
 from .celery import app
-#13456
+#qwq45qqwr
 lock = threading.Lock()
 kiev = timezone('Europe/Kiev')
 logger = logging.getLogger('main')
