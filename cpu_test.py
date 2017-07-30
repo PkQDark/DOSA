f27893e89ec782dbcc1c9fc65f00bad07aabdfd3
PkQDark <blackbirdvlad@gmail.com>
2017-07-30 21:32:55 +0300

    cpu_test

--- cpu_test.py
+++ cpu_test.py
@@ -9,11 +9,10 @@ def test():
     for i in range(1000):
         time.sleep(3)
         for proc in psutil.process_iter():
-            if proc.name() == PROCNAME1 or PROCNAME2:
+            if proc.name() == (PROCNAME1 or PROCNAME2):
                 a = proc.pid
                 p = psutil.Process(a)
                 print(proc.name())
-                print(p.status)
                 print(p.cpu_percent(interval=1))
                 print(p.cpu_affinity())
 
