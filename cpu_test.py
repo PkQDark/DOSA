6914b1cff5b4754f122c5322cb3541d866b10d89
PkQDark <blackbirdvlad@gmail.com>
2017-07-31 02:10:50 +0300

    edit cpu test

--- cpu_test.py
+++ cpu_test.py
@@ -6,17 +6,17 @@ PROCNAME = "celery"
 
 
 def test():
-    cpu = 5.0
-    report = open("cpu_report.txt", "w")
+    cpu = 1.0
     while cpu <= 100.0:
+        report = open("cpu_report.txt", "w")
         for proc in psutil.process_iter():
             if proc.name() == PROCNAME:
                     a = proc.pid
                     p = psutil.Process(a)
                     cpu_lvl = p.cpu_percent(interval=1)
                     if cpu_lvl >= cpu:
-                        report.write("Name process " + p.name() + " cpu = " + cpu_lvl + " date and time = " + str(datetime.datetime.now()) + "\n")
+                        report.write("Cpu = " + cpu_lvl + " date and time = " + str(datetime.datetime.now()) + "\n")
         time.sleep(60)
-    report.close()
+        report.close()
 
 test()
