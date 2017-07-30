0d2e86c2344edf8c4e9f6064cb7043406fba0d15
PkQDark <blackbirdvlad@gmail.com>
2017-07-30 21:28:05 +0300

    cpu_test

--- cpu_test.py
+++ cpu_test.py
@@ -1,7 +1,18 @@
 import psutil
 import time
 
+PROCNAME1 = "celery"
+PROCNAME2 = "rabbitmq"
 
-for i in range(1000):
-    print(psutil.cpu_percent(interval=1))
-    time.sleep(5)
+
+def test():
+    for i in range(1000):
+        time.sleep(3)
+        for proc in psutil.process_iter():
+            if proc.name() == PROCNAME1 or PROCNAME2:
+                a = proc.pid
+                p = psutil.Process(a)
+                print(proc.name())
+                print(p.status)
+                print(p.cpu_percent(interval=1))
+                print(p.cpu_affinity())
